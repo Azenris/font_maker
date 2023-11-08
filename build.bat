dcc1cc6f220818bd0ce7f34f04b0663fba8a55ee
Azenris <morleyx22@hotmail.com>
2023-11-08 00:11:49 +0000

    Update

--- build.bat
+++ build.bat
@@ -7,12 +7,14 @@ cls
 :: run the bootstrap-vcpkg (a vcpkg.exe will be created)
 :: add this to global enviroment paths
 
+:: Then run:
 :: vcpkg install zlib:x64-windows
+:: This will create both a static and shared library (I don't think zlib allows selection)
+:: It will default to finding the shared lib, so we will manually setup the ZLIB_* defines
 
+:: If you want to use the shared lib. then instead of the 3 defines just setup this one
 :: Set CMAKE_TOOLCHAIN_FILE to the <vcpkg-path>/scripts/buildsystems/vcpkg.cmake
 :: (for example) -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake
-
-:: ZLIB (zlib doesn't allow selection of static/dynamic lib. Manually setting them will allow choosing)
 
 if not exist build (
 	cmake -S . -B build ^
